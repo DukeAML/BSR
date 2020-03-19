50edea9d04a560796b3244f80c401aa15804f45c
Nicholas Hobart <nicholas.hobart@duke.edu>
2020-03-19 15:51:54 -0700

    Revised data retrieval method

--- retrievedata.py
+++ retrievedata.py
@@ -2,9 +2,10 @@ import requests
 import json
 
 API_KEY = '44ea3bf2fd0d37eb6d39b576846c69187ffe34fd39177373'
+headers = {'Content-Type': 'application/json'}
 payload = {'token': API_KEY}
 
-r = requests.get('https://app.getsweet.com/api/v1/products.json', params=payload)
+r = requests.get('https://app.getsweet.com/api/v1/products', headers=headers, params=payload)
 
 def jprint(obj):
     """Converts retrieved json files into legible print format

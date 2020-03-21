448d1da5fbeedb4388bcb68b71539bddf7cb86ef
Emily Mittleman <emily.mittleman@duke.edu>
2020-03-20 23:34:21 -0400

    made income csv file

--- data/getincome.py
+++ data/getincome.py
@@ -2,36 +2,39 @@ import requests
 import json
 import pandas as pd
 
-'''
-    "due_date": "2019-05-06"    -- date BSR is paid
-    "payment_total": "270"
-    "item_total": "270"           -- doesn't include shipping
-    "line_items": [
-                {
-                    "id": 508247,
-                    "name": "WHOLESALE-INACTIVE",
-                    "price": 54.0,
-                    "quantity": 5.0,
-                    "variant_id": 15544,
-                    "weight_in_ounces": 192.0
-                }
-            ]
-'''
-
 API_KEY = '44ea3bf2fd0d37eb6d39b576846c69187ffe34fd39177373'
 headers = {'Content-Type': 'application/json'}
-payload = {'token': API_KEY}
 URL = 'https://app.getsweet.com/api/v1/orders.json'
 
-r = requests.get(url=URL, headers=headers, params=payload)
-data = r.json()
-
 columns = ["Due Date", "Invoice Date", "Order #", "Company Name", "Company ID", "Money Paid", "Items"]
+allOrders = []
 
+def extractData(data):
+    global allOrders
+    orders = data["orders"]   #orders is a list of every order
+    
+    for order in orders:        # order is a dict
+        # get "Company Name" & "Company ID"
+        accountInfo = order["account"]
+        company_name = accountInfo["fully_qualified_name"]
+        company_id = accountInfo["id"]
+        # get "Due Date", "Invoice Date", "Order #", & "Money Paid"
+        due_date = order["due_date"]
+        invoice_date = order["invoice_date"]
+        order_num = order["number"]
+        payment_total = order["payment_total"]
+        # get "Items"
+        itemsInfo = order["line_items"]
+        items = []
+        for item in itemsInfo:
+            ID = item["id"]
+            variantID = item["variant_id"]
+            quantity = item["quantity"]
+            items.append({ID : [variantID, quantity]})
 
-def extractData():
-    data
-
+        # zip together the columns with the values
+        values = [due_date, invoice_date, order_num, company_name, company_id, payment_total, items]
+        allOrders.append(dict(zip(columns, values)))
 
 
 def jprint(obj):
@@ -39,27 +42,37 @@ def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
-jprint(data)
+def apiCall_orders(pageNum):
+    payload = {'token': API_KEY, 'page':pageNum}
+    r = requests.get(url=URL, headers=headers, params=payload)
+    data = r.json()
+    return data
 
 
+def main():
+    global allOrders
+
+    data = apiCall_orders(1)
+    meta = data['meta']
+    totalPages = meta['total_pages']
+
+    print(totalPages)
+    totalPages = 1
+    
+    for pageNum in range(1, totalPages+1):
+        data = apiCall_orders(pageNum)
+
+        #jprint(data)
+        extractData(data)
+
+    # create pandas dataframe
+    df = pd.DataFrame(allOrders)
+    df.to_csv (r'income_data.csv', index = False, header=True)
+    print(len(df.index))    #tells you how many orders there are
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    main()
 
 
 

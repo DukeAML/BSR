492bf15a8e71dffbf26c2252a8cd3583b4c5f44a
Emily Mittleman <emily.mittleman@duke.edu>
2020-03-22 23:53:41 -0400

    Created purchases csv

--- data/getExpenses.py
+++ data/getExpenses.py
@@ -2,73 +2,80 @@ import requests
 import json
 import pandas as pd
 import time
-
-
-API_KEY = '44ea3bf2fd0d37eb6d39b576846c69187ffe34fd39177373'
-headers = {'Content-Type': 'application/json'}
-URL = 'https://app.getsweet.com/api/v1/products.json'
-
-#columns = ["Due Date", "Invoice Date", "Order #", "Company Name", "Company ID", "Money Paid", "Items"]
-allOrders = []
-
-def extractData(data):
-    global allOrders
-    products = data["products"]   #orders is a list of every order
-    
-    for product in products:        # order is a dict
-        active = product["active"]
-        if(active == "false"):
-            .
-        
-        due_date = order["due_date"]
-        invoice_date = order["invoice_date"]
-        order_num = order["number"]
-        company_name = order["account"]["fully_qualified_name"]
-        company_id = order["account"]["id"]
-        payment_total = order["payment_total"]
-        items = []
-        for item in order["line_items"]:
-            items.append([item["id"], item["variant_id"], item["quantity"]])
-        # zip together the columns with the values
-        values = [due_date, invoice_date, order_num, company_name, company_id, payment_total, items]
-        allOrders.append(dict(zip(columns, values)))
-
+from csv import writer
+import pathlib
+import numpy as np
 
 def jprint(obj):
     ''' Converts retrieved json files into legible print format '''
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
-
-def apiCall(pageNum):
-    #'q[name_eq]': "INGREDIENTS"
-    # 17669
-    payload = {'token': API_KEY, 'page':pageNum, 'q[name_eq]': "INGREDIENTS"}
-    r = requests.get(url=URL, headers=headers, params=payload)
-    data = r.json()
-    return data
-
-
 def main():
-    global allOrders
+    # filter the csv so it contains purchases only ("Big Spoon Roasters Production Facility Quantity" > 0)
+    PATH = pathlib.Path(__file__).parent
+    df = pd.read_csv(PATH.joinpath("Detail_Inventory_Report_2020-03-22.csv"), low_memory=False)
     
-    data = apiCall(1)
-    totalPages = data['meta']['total_pages']
-
-    totalPages = 1
+    dfP = df[(df["Big Spoon Roasters Production Facility Quantity"].isna()) | (df["Big Spoon Roasters Production Facility Quantity"] > 0)]
+    dfP = dfP.drop(columns=["Transaction Type", "Number", "Customer / Reason", "Customer Type", "Big Spoon Roasters Production Facility QOH", "Quantity Total", "QOH Total"])
     
-    for pageNum in range(1, totalPages+1):
-        data = apiCall(pageNum)
+    # go through every element in date column & get indices of beginning balance
+    sku_indices = []
+    for i in range(len(dfP.index)):
+        if (dfP.iat[i, 0] == "Beginning Balance"):
+            sku_indices.append(i-1)
+        elif(str(dfP.iat[i, 0]).startswith("Total for")):
+            sku_indices.append(i)
 
-        #jprint(data)
-        extractData(data)
+    # add a new column
+    dfP["sku"] = ""
+    dfP["money spent"] = np.nan
 
-    # create pandas dataframe
-    df = pd.DataFrame(allOrders)
-    #df.to_csv (r'income_data1.csv', index = False, header=True)
-    print(len(df.index))    #tells you how many orders there are
+    for i in range(0, len(sku_indices), 2):
+        skuIndex = sku_indices[i]
+        endIndex = sku_indices[i+1]
+        # clean up sku number
+        sku = str(dfP.iat[skuIndex, 0])
+        sku = sku.split(" ")[0]
+        for j in range(skuIndex+2, endIndex):
+            dfP.iat[j, 2] = sku
+
+    # filter so each row is only date, amount, sku
+    dfP = dfP[dfP["sku"] != ""]
+
+    # rename columns
+    dfP.columns = ['date', 'amount bought', 'sku', 'money spent']
+    dfP.to_csv (r'purchases.csv', index = False, header=True)
     
+    dfInfo = pd.read_csv(PATH.joinpath("product_info.csv"), low_memory=False)
 
+    skuNums = dfInfo['sku'].tolist()
+    unitPrices = dfInfo['cost_price'].tolist()
+    dictionary = dict(zip(skuNums, unitPrices))
+
+    print(dictionary)
+    
+    for i in range(len(dfP.index)):
+        sku = dfP.iat[i, 2]
+
+        #unitPrice = float(dfP.at[str(sku), "cost_price"])
+        #newDF = dfInfo.loc[dfInfo['sku'] == sku]
+        #print(newDF)
+        
+        #unitPrice = newDF.iat[0, 1]
+        #unitPrice = dictionary[sku]
+        unitPrice = dictionary.get(sku, 'not here')
+        
+        amountBought = float(dfP.iat[i, 1])
+        if(unitPrice != 'not here'):
+            dfP.iat[i, 3] = amountBought * unitPrice
+
+    dfP.to_csv (r'purchases.csv', index = False, header=True)
+    
 if __name__ == '__main__':
     main()
+
+
+
+
 
---
+++ data/getproductinfo.py
@@ -0,0 +1,76 @@
+import requests
+import json
+import pandas as pd
+import time
+
+'''
+    for pack_size, if == "Each", really means 1
+'''
+
+API_KEY = '44ea3bf2fd0d37eb6d39b576846c69187ffe34fd39177373'
+headers = {'Content-Type': 'application/json'}
+URL = 'https://app.getsweet.com/api/v1/products.json'
+
+#columns = ["Due Date", "Invoice Date", "Order #", "Company Name", "Company ID", "Money Paid", "Items"]
+columns = ["sku", "cost_price", "fully_qualified_name", "active", "incremental_order_quantity", "pack_size", "pack_size_qty", "weight", "weight_units"]
+#columns = ["sku", "cost_price", "pack_size"] 
+allProducts = []
+
+def extractData(data):
+    global allOrders
+    products = data["products"]   #orders is a list of every order
+    
+    for product in products:        # order is a dict
+        variants = product["variants"]
+        for variant in variants:
+            sku = variant["sku"].strip(" ")
+            cost_price = variant["cost_price"]
+            # extras
+            fully_qualified_name = variant["fully_qualified_name"]
+            active = variant["active"]
+            incremental_order_quantity = variant["incremental_order_quantity"]
+            pack_size = variant["pack_size"]
+            pack_size_qty = variant["pack_size_qty"]
+            weight = variant["weight"]
+            weight_units = variant["weight_units"]
+            # make a list of all these values
+            values = [sku, cost_price, fully_qualified_name, active, incremental_order_quantity, pack_size, pack_size_qty, weight, weight_units]
+            allProducts.append(dict(zip(columns, values)))
+
+
+def jprint(obj):
+    ''' Converts retrieved json files into legible print format '''
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+
+def apiCall(pageNum):
+    #'q[name_eq]': "INGREDIENTS"  product_type = inventory_item
+    # 17669
+    payload = {'token': API_KEY, 'page':pageNum, 'q[product_type_eq]': "inventory_item"}
+    r = requests.get(url=URL, headers=headers, params=payload)
+    data = r.json()
+    return data
+
+
+def main():
+    global allOrders
+    
+    data = apiCall(1)
+    totalPages = data['meta']['total_pages']
+    
+    for pageNum in range(1, totalPages+1):
+        data = apiCall(pageNum)
+
+        #jprint(data)
+        extractData(data)
+
+    # create pandas dataframe
+    df = pd.DataFrame(allProducts)
+    df.to_csv (r'product_info.csv', index = False, header=True)
+    print(len(df.index))    #tells you how many orders there are
+    
+
+if __name__ == '__main__':
+    main()
+

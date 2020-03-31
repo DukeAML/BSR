a5c1612f20819b103e367a49880b66b97a115e2c
Phoebe Klett <plk6@duke.edu>
2020-03-31 16:44:44 -0400

    create recipes df for nb

--- translate_sales.py
+++ translate_sales.py
@@ -1,14 +1,26 @@
+import pandas as pd 
 
 #build df with all products for sale
-to_sell = df
+pi = pd.read_csv('product_info.csv')  
+to_sell = pd.DataFrame(pi)
+
+#create nb recipes df
+nbr = pd.read_csv('nb_recipes.csv')  
+nb_recipes = pd.DataFrame(nbr)
+nb_recipes = nb_recipes.drop([0,1,28,29,30,31,32,33,34,43,44]).drop(columns = ['Unnamed: 0', 'Unnamed: 1'])
+columns = ['Peanut', 'Peanut Pecan', 'Peanut Almond', 'Cashew Butter', 'Peanut Cocoa', 'Chai Spice', 'Almond Butter', 'Fiji Ginger', 'GGET Espresso', 'Chocolate Sea Salt', 'Mamba', 'Toasted Coconut', 'Just Peanut', 'Just Almond', 'Maple Cinnamon', 'PCASH', 'WAG', 'LCASH', 'CCCG', 'Twalk']
+index = ['Kosher Sea Salt (lb)', 'Organic Honey (lb)', 'Organic Coconut Oil (lb)', 'RPS- Conventional roasted split peanuts (lb)', 'Pecans - Large Pieces (lb)', 'Dry Roasted Mission Type Almonds (lb)', 'Organic Cashew Kernels LWP (lb)', 'Chai Spice Mix - No Sugar (lb)', 'TCHO Chocolate - 81% Drops (lb)',  'Lemon Powder (lb)','Cocoa Nibs, Bulk (lb)', 'Minor Monuments Espresso (lb)', 'Crystallized Ginger Mini Chips 2-5 mm (lb)', 'Guajillo Chile Powder (lb)', 'Habanero Chile Powder (lb)',  'Ancho Chile Powder, Bulk (lb)', 'Unsweetened Coconut Chips (lb)',  'Coconut Crystals (lb)', 'Sugar, Raw Demerara (lb)', 'Vanilla Powder, Bulk (lb)', 'Hazelnut Kernels (lb)', 'Vietnamese Cinnamon, Ground (lb)', 'Organic Maple Powder (lb)', 'Organic Maple Granules (lb)', 'TCHO Chocolate - 81% Drops (lb)', 'Organic Chia Seeds (lb)', 'Total Weight (lbs)', 'Total Weight (oz)', '13oz', '10oz', '3oz', '1lb', '4lb', '8lb']
+nb_recipes.columns = columns
+nb_recipes.index = index
+
 
 #build df with base products each product implies
 #columns = [product_name, sku, level_down = [(base product, sku, num_needed)]]
-products = df
+#products = df
 
 #build df with ingredients each base product implies
 #columns = [base_prodct_name, sku, level_down = [(ingredient name, sku, amt_needed)]]
-base_products = df
+#base_products = df
 
 
 #pull orders 

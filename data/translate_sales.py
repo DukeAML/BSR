01ebc41bff1622c029fe111a65d75738f752821b
Phoebe Klett <plk6@duke.edu>
2020-04-01 12:46:24 -0400

    pulling data and organizing

--- data/translate_sales.py
+++ data/translate_sales.py
@@ -1,36 +1,36 @@
 import pandas as pd 
 
 #build df with all products for sale
-pi = pd.read_csv('products_forsale.csv')  
+pi = pd.read_csv('product_info.csv')  
 to_sell = pd.DataFrame(pi)
-to_sell["Level Down: Base Products"] = [] #fix this next 
-print(to_sell["fully_qualified_name"][400:])
 
 #create nb recipes df
 nbr = pd.read_csv('nb_recipes.csv')  
 nb_recipes = pd.DataFrame(nbr)
-nb_recipes = nb_recipes.drop([26,27,28,29,30,39,40]).drop(columns = ['Ingredient', 'Unit'])
+nb_recipes = nb_recipes.drop([0,1,28,29,30,31,32,33,34,43,44]).drop(columns = ['Unnamed: 0', 'Unnamed: 1'])
+columns = ['Peanut', 'Peanut Pecan', 'Peanut Almond', 'Cashew Butter', 'Peanut Cocoa', 'Chai Spice', 'Almond Butter', 'Fiji Ginger', 'GGET Espresso', 'Chocolate Sea Salt', 'Mamba', 'Toasted Coconut', 'Just Peanut', 'Just Almond', 'Maple Cinnamon', 'PCASH', 'WAG', 'LCASH', 'CCCG', 'Twalk']
 index = ['Kosher Sea Salt (lb)', 'Organic Honey (lb)', 'Organic Coconut Oil (lb)', 'RPS- Conventional roasted split peanuts (lb)', 'Pecans - Large Pieces (lb)', 'Dry Roasted Mission Type Almonds (lb)', 'Organic Cashew Kernels LWP (lb)', 'Chai Spice Mix - No Sugar (lb)', 'TCHO Chocolate - 81% Drops (lb)',  'Lemon Powder (lb)','Cocoa Nibs, Bulk (lb)', 'Minor Monuments Espresso (lb)', 'Crystallized Ginger Mini Chips 2-5 mm (lb)', 'Guajillo Chile Powder (lb)', 'Habanero Chile Powder (lb)',  'Ancho Chile Powder, Bulk (lb)', 'Unsweetened Coconut Chips (lb)',  'Coconut Crystals (lb)', 'Sugar, Raw Demerara (lb)', 'Vanilla Powder, Bulk (lb)', 'Hazelnut Kernels (lb)', 'Vietnamese Cinnamon, Ground (lb)', 'Organic Maple Powder (lb)', 'Organic Maple Granules (lb)', 'TCHO Chocolate - 81% Drops (lb)', 'Organic Chia Seeds (lb)', 'Total Weight (lbs)', 'Total Weight (oz)', '13oz', '10oz', '3oz', '1lb', '4lb', '8lb']
+nb_recipes.columns = columns
 nb_recipes.index = index
 
-#create bar recipes df
 br = pd.read_csv('bar_recipes.csv')  
 bar_recipes = pd.DataFrame(br)
-bar_recipes = bar_recipes.drop(columns = ['Unnamed: 1','Unnamed: 11','NEW #1','Unnamed: 17' ,'Unnamed: 10','Unnamed: 2','Unnamed: 4','Unnamed: 5', 'Unnamed: 18','NEW #2','Unnamed: 16','Unnamed: 13','Unnamed: 19','Unnamed: 8' ,'Unnamed: 20', 'Unnamed: 7','Unnamed: 14'])
-bar_recipes = bar_recipes.fillna(0)
+print(bar_recipes)
+
+# bar_recipes = nb_recipes.drop([0,1,28,29,30,31,32,33,34,43,44]).drop(columns = ['Unnamed: 0', 'Unnamed: 1'])
+# columns = ['Peanut', 'Peanut Pecan', 'Peanut Almond', 'Cashew Butter', 'Peanut Cocoa', 'Chai Spice', 'Almond Butter', 'Fiji Ginger', 'GGET Espresso', 'Chocolate Sea Salt', 'Mamba', 'Toasted Coconut', 'Just Peanut', 'Just Almond', 'Maple Cinnamon', 'PCASH', 'WAG', 'LCASH', 'CCCG', 'Twalk']
+# index = ['Kosher Sea Salt (lb)', 'Organic Honey (lb)', 'Organic Coconut Oil (lb)', 'RPS- Conventional roasted split peanuts (lb)', 'Pecans - Large Pieces (lb)', 'Dry Roasted Mission Type Almonds (lb)', 'Organic Cashew Kernels LWP (lb)', 'Chai Spice Mix - No Sugar (lb)', 'TCHO Chocolate - 81% Drops (lb)',  'Lemon Powder (lb)','Cocoa Nibs, Bulk (lb)', 'Minor Monuments Espresso (lb)', 'Crystallized Ginger Mini Chips 2-5 mm (lb)', 'Guajillo Chile Powder (lb)', 'Habanero Chile Powder (lb)',  'Ancho Chile Powder, Bulk (lb)', 'Unsweetened Coconut Chips (lb)',  'Coconut Crystals (lb)', 'Sugar, Raw Demerara (lb)', 'Vanilla Powder, Bulk (lb)', 'Hazelnut Kernels (lb)', 'Vietnamese Cinnamon, Ground (lb)', 'Organic Maple Powder (lb)', 'Organic Maple Granules (lb)', 'TCHO Chocolate - 81% Drops (lb)', 'Organic Chia Seeds (lb)', 'Total Weight (lbs)', 'Total Weight (oz)', '13oz', '10oz', '3oz', '1lb', '4lb', '8lb']
+# nb_recipes.columns = columns
+# nb_recipes.index = index
 
 
 #build df with base products each product implies
 #columns = [product_name, sku, level_down = [(base product, sku, num_needed)]]
-
-# pr_to_bpr = pd.DataFrame(columns = ['Product Name','sku','Level Down: Base Products'])
-# pr_tobpr[]
+#products = df
 
 #build df with ingredients each base product implies
 #columns = [base_prodct_name, sku, level_down = [(ingredient name, sku, amt_needed)]]
 #base_products = df
-
-# bpr_to_ingredients = pd.DataFrame(columns = ['Base Product Name','sku','Level Down: Ingredients'])
 
 
 #pull orders 

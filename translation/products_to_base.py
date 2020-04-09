a60d602f52f64a64c38d39ece883061a06985bc6
Phoebe Klett <plk6@duke.edu>
2020-04-09 13:28:05 -0400

    finished base csv

--- translation/products_to_base.py
+++ translation/products_to_base.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+import ast
 
 temp = pd.read_csv('base.csv')  
 products = pd.DataFrame(temp)
@@ -39,7 +40,7 @@ class Order:
 
             for ii in range(r):
                 t = c[n:(2*ii)+2]
-                print(t)
+                
                 sku = t[0].strip(' () ')
                 num = int(t[1].strip(' () '))
                 n +=2
@@ -56,17 +57,54 @@ order = Order(['NB-ALM-13OZ-1PACK', 'NB-ALM-13OZ-2PACK', 'NB-CASH-13OZ-1PACK','N
 order.get_base()
 base = order.base
 
+new_amt = pd.DataFrame()
+new_amt["sku"] = base.keys()
+new_amt["amt"] = base.values()
 
-implied_base = pd.DataFrame()
-implied_base["sku"] = base.keys()
-implied_base["amt"] = base.values()
-print(implied_base)
 
-for sku in implied_base["sku"]:
-    #look up in csv with their current stock -- make this analogous to products.csv
-    #get col of how many we have
-    #subtract from amt col here make a new col --> amt we have left 
 
-    #final df
-    #sku  amt_need  amt_have  amt_left
+temp2 = pd.read_csv('base-products.csv')  
+base_products = pd.DataFrame(temp2)
+
+skus = base_products["sku"]
+skus = [item[1] for item in skus.items()]
+skus = [item.strip(" '' ") for item in skus]
+
+helperA ={}
+helperB= {}
+
+for sku in skus: 
+    stock = base_products.loc[base_products['sku'] == sku]["stock"]
+    stock = stock.reset_index().drop(columns = ["index"])
+    stock = stock.lookup(row_labels =[0], col_labels=["stock"])
+    stock = stock.tolist()[0]
+    stock = stock.strip("[]")
+    stock = ast.literal_eval(stock)
+
+    have = float(stock["on_hand"]) #should this be available or on hand?
+    committed = float(stock["committed"])
+
+    helperA[sku] = have
+    helperB[sku] = committed
+
+df = pd.DataFrame()
+df["sku"] = helperA.keys()
+df["available"] = helperA.values()
+df["prev committed"] = helperB.values()
+
+new_amt = []
+for sku in skus:
+    if sku in base:
+        new_amount = base[sku]
+    elif sku not in base:
+        new_amount = helperB[sku]
+    new_amt.append(new_amount)
+
+df["updated committed"] = new_amt
+df["left over"] = df["available"] - df["updated committed"]
+df["name"] = base_products["fully_qualified_name"]
+df.to_csv('updated_baseinventory.csv', index = False)
+
+
+
 

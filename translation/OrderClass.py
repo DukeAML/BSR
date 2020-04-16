2e86587fb620a75e75aa6deafe9e0ee46cd9dc54
Phoebe Klett <plk6@duke.edu>
2020-04-16 17:32:17 -0400

    added other levels

--- translation/OrderClass.py
+++ translation/OrderClass.py
@@ -20,6 +20,10 @@ class Order:
         self.amts = dictionary.values()
         # self.length = len(lst_skus)
         self.level_one = None
+        self.level_two = None
+        self.level_three = None
+        self.level_four = None
+        self.level_five = None
 
     def get_level_one(self):
         '''
@@ -60,4 +64,161 @@ class Order:
         self.level_one = base
         return
     
+
+    def get_level_two(self):
+        '''
+        get_base: this method gets the base products for the order
+
+        '''
+        base = {}
+        for sku in self.skus: # for product in order get base products
+            bp = products.loc[products['sku'] == sku]["Level Two"]
+            bp = bp.reset_index().drop(columns = ["index"])
+            
+            if bp.empty == True:
+                print("unknown sku")
+                print(sku)
+                
+            else:
+                bp = bp.lookup(row_labels =[0], col_labels=["Level Two"])
+
+            temp = str(bp)
+            a = temp.strip(' [] ').strip(' " ')
+            b = a.strip('[]')
+            c = b.split(",")
+            l = len(c) 
+            r = l //2  
+            n=0
+
+            for ii in range(r):
+                t = c[n:(2*ii)+2]
+                sku = t[0].strip(' () ')
+                num = int(t[1].strip(' () '))
+                n +=2
+
+                if sku not in base:
+                    base[sku] = num #add to dict
+                else: 
+                    base[sku] += num
+
+        self.level_two = base
+        return
+    
+    def get_level_three(self):
+        '''
+        get_base: this method gets the base products for the order
+
+        '''
+        base = {}
+        for sku in self.skus: # for product in order get base products
+            bp = products.loc[products['sku'] == sku]["Level Three"]
+            bp = bp.reset_index().drop(columns = ["index"])
+            
+            if bp.empty == True:
+                print("unknown sku")
+                print(sku)
+                
+            else:
+                bp = bp.lookup(row_labels =[0], col_labels=["Level Three"])
+
+            temp = str(bp)
+            a = temp.strip(' [] ').strip(' " ')
+            b = a.strip('[]')
+            c = b.split(",")
+            l = len(c) 
+            r = l //2  
+            n=0
+
+            for ii in range(r):
+                t = c[n:(2*ii)+2]
+                sku = t[0].strip(' () ')
+                num = int(t[1].strip(' () '))
+                n +=2
+
+                if sku not in base:
+                    base[sku] = num #add to dict
+                else: 
+                    base[sku] += num
+
+        self.level_three = base
+        return
+   
+    
+    def get_level_four(self):
+        '''
+        get_base: this method gets the base products for the order
+
+        '''
+        base = {}
+        for sku in self.skus: # for product in order get base products
+            bp = products.loc[products['sku'] == sku]["Level Four"]
+            bp = bp.reset_index().drop(columns = ["index"])
+            
+            if bp.empty == True:
+                print("unknown sku")
+                print(sku)
+                
+            else:
+                bp = bp.lookup(row_labels =[0], col_labels=["Level Four"])
+
+            temp = str(bp)
+            a = temp.strip(' [] ').strip(' " ')
+            b = a.strip('[]')
+            c = b.split(",")
+            l = len(c) 
+            r = l //2  
+            n=0
+
+            for ii in range(r):
+                t = c[n:(2*ii)+2]
+                sku = t[0].strip(' () ')
+                num = int(t[1].strip(' () '))
+                n +=2
+
+                if sku not in base:
+                    base[sku] = num #add to dict
+                else: 
+                    base[sku] += num
+
+        self.level_four = base
+        return
  
+
+    def get_level_five(self):
+        '''
+        get_base: this method gets the base products for the order
+
+        '''
+        base = {}
+        for sku in self.skus: # for product in order get base products
+            bp = products.loc[products['sku'] == sku]["Level Five"]
+            bp = bp.reset_index().drop(columns = ["index"])
+            
+            if bp.empty == True:
+                print("unknown sku")
+                print(sku)
+                
+            else:
+                bp = bp.lookup(row_labels =[0], col_labels=["Level Five"])
+
+            temp = str(bp)
+            a = temp.strip(' [] ').strip(' " ')
+            b = a.strip('[]')
+            c = b.split(",")
+            l = len(c) 
+            r = l //2  
+            n=0
+
+            for ii in range(r):
+                t = c[n:(2*ii)+2]
+                sku = t[0].strip(' () ')
+                num = int(t[1].strip(' () '))
+                n +=2
+
+                if sku not in base:
+                    base[sku] = num #add to dict
+                else: 
+                    base[sku] += num
+
+        self.level_five = base
+        return
--- translation/TranslateOrders.py
+++ translation/TranslateOrders.py
@@ -78,3 +78,4 @@ def order_translation(orders):
 
 
 
+

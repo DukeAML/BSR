7a917c3a50ca206c9a70b9d3b478c9504d7d83e7
Emily Mittleman <emily.mittleman@duke.edu>
2020-05-19 22:32:00 -0400

    created getIngredients to make csv of only ingredients

--- data/getIngredients.py
+++ data/getIngredients.py
@@ -3,17 +3,21 @@ import json
 import pandas as pd
 import time
 
+'''
+    generates ingredients.csv, which is a csv of ingredients only
+'''
+
 
 API_KEY = '44ea3bf2fd0d37eb6d39b576846c69187ffe34fd39177373'
 headers = {'Content-Type': 'application/json'}
 URL = 'https://app.getsweet.com/api/v1/products.json'
 
-columns = ["sku", "cost_price", "fully_qualified_name", "active", "incremental_order_quantity", "pack_size", "pack_size_qty","stock", "weight", "weight_units"]
+columns = ["sku", "ID", "cost_price", "fully_qualified_name", "active", "incremental_order_quantity",
+           "pack_size", "pack_size_qty","stock", "weight", "weight_units", "asset_account_id", "is_master"]
 allProducts = []
 
 def extractData(data):
     global allOrders
-    print(data)
 
     products = data["products"]   #orders is a list of every order
 
@@ -22,6 +26,7 @@ def extractData(data):
         # print(variants)
         for variant in variants:
             sku = variant["sku"].strip(" ")
+            ID = variant["id"]
             fully_qualified_name = variant["fully_qualified_name"]
             active = variant["active"]
             cost_price = variant["cost_price"]
@@ -31,10 +36,13 @@ def extractData(data):
             weight = variant["weight"]
             weight_units = variant["weight_units"]
             stock = variant["stock_items"]
-
+            asset_account_id = variant["asset_account_id"]
+            is_master = variant["is_master"]
+            
             # make a list of all these values
-            values = [sku, cost_price, fully_qualified_name, active, incremental_order_quantity, pack_size, pack_size_qty, stock, weight, weight_units]
-            allProducts.append(dict(zip(columns, values)))
+            values = [sku, ID, cost_price, fully_qualified_name, active, incremental_order_quantity,
+                      pack_size, pack_size_qty, stock, weight, weight_units, asset_account_id, is_master]
+            allProducts.append(values)
 
 
 def jprint(obj):
@@ -47,10 +55,8 @@ def jprint(obj):
 
 
 def apiCall(pageNum):
-    ''' Returns the products that == inventory_item (rather than inventory_assembly) '''
-    #'q[name_eq]': "INGREDIENTS"
-    # payload = {'token': API_KEY, 'page':pageNum, 'q[product_type_eq]': "inventory_assembly"}
-    payload = {'token': API_KEY, 'page':pageNum}
+    ''' Returns the products that == inventory_item '''
+    payload = {'token': API_KEY, 'page':pageNum, 'q[product_type_eq]': "inventory_item"}
     r = requests.get(url=URL, headers=headers, params=payload)
     data = r.json()
     return data
@@ -67,10 +73,16 @@ def main():
         extractData(data)
 
     # create pandas dataframe
-    df = pd.DataFrame(allProducts)
+    df = pd.DataFrame(allProducts, columns = columns)
+
+    # only keep products that are active
+    df = df[df["active"] == True]
+
+    # out of all inventory items, only ones w/ asset_account_id == 1017 are ingredients
+    df = df[df["asset_account_id"] == 1017]
 
     # write to csv
-    df.to_csv (r'inventory3.csv', index = False, header=True)
+    df.to_csv (r'ingredients.csv', index = False, header=True)
     
 
 if __name__ == '__main__':
